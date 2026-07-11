96f9ab87cf28b3851d8be95b42669a7f786157cb
Alan Greenwood <alan@darklab.co.uk>
2026-07-11 18:52:21 +0100

    fix: launch on startup (windows)

--- win/src/StartupRegistration.cs
+++ win/src/StartupRegistration.cs
@@ -7,7 +7,13 @@ namespace Sanity
     public static class StartupRegistration
     {
         private const string RunKeyPath = @"Software\Microsoft\Windows\CurrentVersion\Run";
+        private const string StartupApprovedKeyPath =
+            @"Software\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run";
         private const string ValueName = "Sanity";
+
+        // Task Manager / Settings read this binary blob; 0x02 = enabled.
+        private static readonly byte[] EnabledApproval =
+            { 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
         public static bool IsRegistered()
         {
@@ -20,11 +26,14 @@ namespace Sanity
                 if (string.IsNullOrEmpty(value))
                     return false;
 
-                return string.Equals(
+                if (!string.Equals(
                     NormalizePath(value),
                     NormalizePath(Application.ExecutablePath),
-                    StringComparison.OrdinalIgnoreCase);
+                    StringComparison.OrdinalIgnoreCase))
+                    return false;
             }
+
+            return IsStartupApproved();
         }
 
         public static void Apply(bool launchOnStartup)
@@ -36,6 +45,38 @@ namespace Sanity
 
                 if (launchOnStartup)
                     key.SetValue(ValueName, "\"" + Application.ExecutablePath + "\"");
+                else
+                    key.DeleteValue(ValueName, false);
+            }
+
+            ApplyStartupApproved(launchOnStartup);
+        }
+
+        private static bool IsStartupApproved()
+        {
+            using (var key = Registry.CurrentUser.OpenSubKey(StartupApprovedKeyPath, false))
+            {
+                if (key == null)
+                    return true;
+
+                var data = key.GetValue(ValueName) as byte[];
+                if (data == null || data.Length == 0)
+                    return true;
+
+                // Odd low nibble (e.g. 0x03) means disabled in Task Manager.
+                return (data[0] & 0x01) == 0;
+            }
+        }
+
+        private static void ApplyStartupApproved(bool launchOnStartup)
+        {
+            using (var key = Registry.CurrentUser.CreateSubKey(StartupApprovedKeyPath, true))
+            {
+                if (key == null)
+                    return;
+
+                if (launchOnStartup)
+                    key.SetValue(ValueName, EnabledApproval, RegistryValueKind.Binary);
                 else
                     key.DeleteValue(ValueName, false);
             }
